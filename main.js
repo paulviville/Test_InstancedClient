fe5762994640c4956500d8d9d9c0bfc2aa420854
paulviville <paul.viville@gmail.com>
2025-11-06 17:01:06 +0100

    fail

--- main.js
+++ main.js
@@ -12,4 +12,5 @@ window.disconnect = () => {clientManager.disconnect()}
 window.requestNewInstance = ( name ) => {clientManager.requestNewInstance( name )}
 window.requestJoinInstance = ( name ) => {clientManager.requestJoinInstance( name )}
 window.requestLeaveInstance = ( name ) => {clientManager.requestLeaveInstance( name )}
+window.requestFileRequest = ( name ) => {clientManager.requestFileRequest( name )}
 window.loadFile = async ( ) => { await clientManager.loadFile( "./Files", "bath_day.glb" )}
